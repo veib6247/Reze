58bed5c87dcfe808b3c7656aa0562340a0e97ac7
Bryan Olandres <ozoneblacklight@outlook.com>
2023-10-06 00:30:35 +0800

    moved page title outside the nuxtpage

--- pages/index/merchantList.vue
+++ pages/index/merchantList.vue
@@ -1,3 +1,9 @@
 <template>
-  <NuxtPage />
+  <div class="w-screen flex flex-col gap-0">
+    <div class="px-4 py-2 sticky top-0 bg-stone-300">
+      <h1 class="text-lg text-gray-800 font-raleway">{{ $route.query.divisionName }}</h1>
+    </div>
+
+    <NuxtPage />
+  </div>
 </template>
--- pages/index/merchantList/[divisionId].vue
+++ pages/index/merchantList/[divisionId].vue
@@ -6,11 +6,7 @@
 </script>
 
 <template>
-  <div class="w-screen flex flex-col gap-0">
-    <div class="px-4 py-2 sticky top-0 bg-stone-300">
-      <h1 class="text-lg text-gray-800 font-raleway">{{ $route.query.divisionName }}</h1>
-    </div>
-
+  <div class="flex flex-col gap-0 overflow-auto">
     <div class="p-6 text-stone-300 font-raleway flex gap-2" v-if="pending">
       <span>
         <IconsLoading />
@@ -20,7 +16,7 @@
       </span>
     </div>
 
-    <div class="p-6 flex flex-wrap gap-2 overflow-auto" v-else>
+    <div class="p-6 flex flex-wrap gap-2 " v-else>
       <MerchantDisplay v-for="merchant in merchantData" :merchantName="merchant.name" :merchantId="merchant.id"
         :merchantState="merchant.state" :merchantDescription="merchant.description" />
     </div>
