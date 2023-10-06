71d11831a3b5a8d68ec83792cb6b7bda70f395b2
Bryan Olandres <ozoneblacklight@outlook.com>
2023-10-06 18:59:23 +0800

    changed hover color

--- components/DivisionItem.vue
+++ components/DivisionItem.vue
@@ -13,7 +13,7 @@
 </script>
 
 <template>
-  <NuxtLink class="px-4 py-1 text-sm text-gray-300 font-raleway hover:bg-stone-400 hover:text-slate-900"
+  <NuxtLink class="px-4 py-1 text-sm text-gray-300 font-raleway hover:bg-stone-300 hover:text-slate-900"
     exact-active-class="bg-stone-300 text-slate-900 hover:bg-stone-300"
     :to="`/merchantList/${divisionId}?divisionName=${divisionName}`">
     <slot />
