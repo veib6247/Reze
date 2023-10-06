4bf195fdf72f5c117a75ffc0254d92b84e02421c
Bryan Olandres <ozoneblacklight@outlook.com>
2023-10-06 18:33:58 +0800

    added channel fetching, fixed display height

--- components/MerchantDisplay.vue
+++ components/MerchantDisplay.vue
@@ -23,18 +23,20 @@
 
   const { pending: createdMerchantAccountsPending, data: createdMerchantAccounts, error: createdMerchantAccountsError } = await useLazyFetch(`/api/getCreatedMerchantAccounts?merchantId=${props.merchantId}`)
 
+  const { pending: channelsPending, data: channelsList } = await useLazyFetch(`/api/getChannel?merchantId=${props.merchantId}`)
+
   // console.info(createdMerchantAccounts.value)
 </script>
 
 <template>
-  <div class="backdrop-blur-sm bg-stone-50/5 w-80 h-96 flex flex-col gap-0 border border-stone-300/60">
-    <h1 class="font-raleway bg-stone-300 p-2 truncate">
+  <div class="backdrop-blur-sm bg-stone-50/5 w-80 h-fit flex flex-col gap-0 border border-stone-300/60">
+    <div class="bg-stone-300 h-10 p-2 font-raleway truncate">
       <span class="text-gray-800">
         {{ merchantName.toUpperCase() }}
       </span>
-    </h1>
+    </div>
 
-    <div class="p-2 flex flex-col gap-2 overflow-auto">
+    <div class="h-96 p-2 flex flex-col gap-4 overflow-y-auto">
       <div class="text-stone-300 text-sm">
         <span class="block text-stone-300/70 text-xs">Mechant Entity ID</span>
         <span class="font-mono">
@@ -65,12 +67,10 @@
 
       <div class="text-stone-300 font-raleway text-sm"
         v-if="!createdMerchantAccountsPending && createdMerchantAccounts.length > 0">
-
-        <span class="block text-stone-300/70 text-xs">Created Merchant Accounts</span>
+        <span class="block text-stone-300/70 text-xs">Merchant Accounts</span>
 
         <div class="flex flex-col gap-0.5 py-1">
-          <span class="bg-stone-300/40 text-stone-300/80 text-xs px-2 py-1 rounded-sm w-fit"
-            v-for="createdMerchantAccount in createdMerchantAccounts">
+          <span class="text-stone-300 font-raleway text-sm" v-for="createdMerchantAccount in createdMerchantAccounts">
             {{ createdMerchantAccount.name }}
           </span>
         </div>
@@ -78,17 +78,32 @@
 
       <div class="flex gap-2" v-else-if="createdMerchantAccountsPending">
         <span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-5 h-5 text-stone-300/70 animate-spin">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-          </svg>
+          <IconsLoadingSmol />
         </span>
         <span class="block text-stone-300/70 text-xs">Fetching Merchant Accounts...</span>
       </div>
 
       <div v-else-if="createdMerchantAccountsError">
         <span class="block text-red-300/80 text-xs">Error in Fetching Merchant Accounts</span>
+      </div>
+
+
+      <!-- Channels list here -->
+      <div class="text-stone-300 font-raleway text-sm" v-if="!channelsPending">
+        <span class="block text-stone-300/70 text-xs">Channels</span>
+
+        <div class="flex flex-col gap-0.5 py-1">
+          <span class="text-stone-300 font-raleway text-sm" v-for="channel in channelsList">
+            {{ channel.name }}
+          </span>
+        </div>
+      </div>
+
+      <div class="flex gap-2" v-else-if="channelsPending">
+        <span>
+          <IconsLoadingSmol />
+        </span>
+        <span class="block text-stone-300/70 text-xs">Fetching Channels List...</span>
       </div>
 
     </div>
---
+++ server/api/getChannel.get.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+
+  const urlEndpoint = `https://test.ctpe.info/bip/webapi/v1/merchants/${query.merchantId}/channels`
+  const requestCredentialHeader = `${process.env.EMAIL}:${process.env.PASSWORD}`
+
+  try {
+    const response = await axios({
+      url: urlEndpoint,
+      method: 'get',
+      headers: {
+        credentials: requestCredentialHeader,
+      },
+    })
+
+    const data = response.data
+    return data.channels
+
+    //
+  } catch (error) {
+    return {
+      SeverError: error,
+    }
+  }
+})
