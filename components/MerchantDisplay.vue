4e98b94b41aa64a9ae66fd2e232e863054dfc9f8
Bryan Olandres <ozoneblacklight@outlook.com>
2023-10-08 13:56:29 +0800

    better loading state handling in template

--- components/MerchantDisplay.vue
+++ components/MerchantDisplay.vue
@@ -23,19 +23,22 @@
 
   const { pending: createdMerchantAccountsPending, data: createdMerchantAccounts, error: createdMerchantAccountsError } = await useLazyFetch(`/api/getCreatedMerchantAccounts?merchantId=${props.merchantId}`)
 
-  const { pending: channelsPending, data: channelsList } = await useLazyFetch(`/api/getChannel?merchantId=${props.merchantId}`)
+  const { pending: channelsPending, data: channelsList, error: channelsError } = await useLazyFetch(`/api/getChannel?merchantId=${props.merchantId}`)
 
   // console.info(createdMerchantAccounts.value)
 </script>
 
 <template>
   <div class="backdrop-blur-sm bg-stone-50/5 w-80 h-fit flex flex-col gap-0 border border-stone-300/60">
+
+    <!-- card title -->
     <div class="bg-stone-300 h-10 p-2 font-raleway truncate">
       <span class="text-gray-800">
         {{ merchantName.toUpperCase() }}
       </span>
     </div>
 
+    <!-- card body -->
     <div class="h-96 p-2 flex flex-col gap-4 overflow-y-auto">
       <div class="text-stone-300 text-sm">
         <span class="block text-stone-300/70 text-xs">Mechant Entity ID</span>
@@ -65,8 +68,15 @@
         </p>
       </div>
 
-      <div class="text-stone-300 font-raleway text-sm"
-        v-if="!createdMerchantAccountsPending && createdMerchantAccounts.length > 0">
+      <!-- merchant list here -->
+      <div class="flex gap-2" v-if="createdMerchantAccountsPending">
+        <span>
+          <IconsLoadingSmol />
+        </span>
+        <span class="block text-stone-300/70 text-xs">Fetching Merchant Accounts...</span>
+      </div>
+
+      <div class="text-stone-300 font-raleway text-sm" v-if="createdMerchantAccounts.length">
         <span class="block text-stone-300/70 text-xs">Merchant Accounts</span>
 
         <div class="flex flex-col gap-0.5 py-1">
@@ -76,20 +86,19 @@
         </div>
       </div>
 
-      <div class="flex gap-2" v-else-if="createdMerchantAccountsPending">
-        <span>
-          <IconsLoadingSmol />
-        </span>
-        <span class="block text-stone-300/70 text-xs">Fetching Merchant Accounts...</span>
-      </div>
-
-      <div v-else-if="createdMerchantAccountsError">
+      <div v-if="createdMerchantAccountsError">
         <span class="block text-red-300/80 text-xs">Error in Fetching Merchant Accounts</span>
       </div>
 
+      <!-- channel list here -->
+      <div class="flex gap-2" v-if="channelsPending">
+        <span>
+          <IconsLoadingSmol />
+        </span>
+        <span class="block text-stone-300/70 text-xs">Fetching Channels List...</span>
+      </div>
 
-      <!-- Channels list here -->
-      <div class="text-stone-300 font-raleway text-sm" v-if="!channelsPending">
+      <div class="text-stone-300 font-raleway text-sm" v-if="channelsList.length">
         <span class="block text-stone-300/70 text-xs">Channels</span>
 
         <div class="flex flex-col gap-0.5 py-1">
@@ -99,13 +108,9 @@
         </div>
       </div>
 
-      <div class="flex gap-2" v-else-if="channelsPending">
-        <span>
-          <IconsLoadingSmol />
-        </span>
-        <span class="block text-stone-300/70 text-xs">Fetching Channels List...</span>
+      <div v-if="channelsError">
+        <span class="block text-red-300/80 text-xs">Error in Fetching Channels List</span>
       </div>
-
     </div>
   </div>
 </template>
