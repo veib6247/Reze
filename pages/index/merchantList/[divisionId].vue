dcc9933b137b59d78feca3679705eeaf1690cf24
Bryan Olandres <ozoneblacklight@outlook.com>
2023-10-08 17:40:18 +0800

    installed tw forms plugin

--- pages/index/merchantList/[divisionId].vue
+++ pages/index/merchantList/[divisionId].vue
@@ -1,12 +1,36 @@
 <script setup lang="ts">
+  type Merchant = {
+    id: string;
+    name: string;
+    state: string;
+    description: string;
+  }
+
   const route = useRoute()
   const { pending: merchantDataPending, data: merchantData, error: merchantDataError } = await useLazyFetch(`/api/getMerchant?divisionId=${route.params.divisionId}`)
 
-  // console.info(merchantData.value)
+  const filterQuery = ref('')
+  const filteredArray: any = ref([])
+
+  watch(filterQuery, (newVal, oldVal) => {
+    console.info(`Filtering for ${newVal}`)
+    filteredArray.value = merchantData.value.filter((merchant: Merchant) => {
+      return merchant.name === newVal
+    })
+  })
 </script>
 
 <template>
   <div class="flex flex-col gap-0 overflow-auto">
+    <!-- search box -->
+    <div class="px-6 pt-6">
+      <div class="backdrop-blur-sm bg-stone-50/5 border border-stone-300/60 p-2">
+        <span class="text-stone-300/70 font-raleway">Search</span>
+        <input type="text" class="p-2 bg-none" v-model="filterQuery" />
+      </div>
+    </div>
+
+
     <div class="p-6 text-stone-300 font-raleway flex gap-2" v-if="merchantDataPending">
       <span>
         <IconsLoading />
@@ -17,8 +41,13 @@
     </div>
 
     <div class="p-6 flex flex-wrap gap-2 " v-else>
-      <TransitionGroup name="list">
+      <TransitionGroup name="list" v-if="!filteredArray.length">
         <MerchantDisplay v-for="merchant in merchantData" :key="merchant.id" :merchantName="merchant.name"
+          :merchantId="merchant.id" :merchantState="merchant.state" :merchantDescription="merchant.description" />
+      </TransitionGroup>
+
+      <TransitionGroup name="list" v-if="filteredArray.length">
+        <MerchantDisplay v-for="merchant in filteredArray" :key="merchant.id" :merchantName="merchant.name"
           :merchantId="merchant.id" :merchantState="merchant.state" :merchantDescription="merchant.description" />
       </TransitionGroup>
     </div>
