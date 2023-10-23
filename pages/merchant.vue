d56eb371d086f394a28d3ad8baf6851e8104138d
Bryan Olandres <ozoneblacklight@outlook.com>
2023-10-23 14:03:33 +0800

    spacing

--- pages/merchant.vue
+++ pages/merchant.vue
@@ -8,30 +8,30 @@
       class="sticky top-0 z-40 flex h-14 shrink-0 gap-4 bg-stone-300 px-4 py-2">
       <div class="flex w-full flex-col gap-0">
         <h1 class="font-raleway text-xs text-gray-800/80">Merchant Name</h1>
-        <span class="font-mono text-sm text-gray-800">{{
-          merchantInfo.detail.name
-        }}</span>
+        <span class="font-mono text-sm text-gray-800">
+          {{ merchantInfo.detail.name }}
+        </span>
       </div>
 
       <div class="flex w-full flex-col gap-0">
         <h1 class="font-raleway text-xs text-gray-800/80">Enity ID</h1>
-        <span class="font-mono text-sm text-gray-800">{{
-          merchantInfo.detail.id
-        }}</span>
+        <span class="font-mono text-sm text-gray-800">
+          {{ merchantInfo.detail.id }}
+        </span>
       </div>
 
       <div class="flex w-full flex-col gap-0">
         <h1 class="font-raleway text-xs text-gray-800/80">State</h1>
-        <span class="font-mono text-sm text-gray-800">{{
-          merchantInfo.detail.state
-        }}</span>
+        <span class="font-mono text-sm text-gray-800">
+          {{ merchantInfo.detail.state }}
+        </span>
       </div>
 
       <div class="flex w-full flex-col gap-0">
         <h1 class="font-raleway text-xs text-gray-800/80">Description</h1>
-        <span class="font-mono text-sm text-gray-800">{{
-          merchantInfo.detail.description
-        }}</span>
+        <span class="font-mono text-sm text-gray-800">
+          {{ merchantInfo.detail.description }}
+        </span>
       </div>
     </div>
 
