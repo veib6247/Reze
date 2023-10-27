86e80f28e48fe53d45eabe48cdc30c01391d870f
Bryan Olandres <ozoneblacklight@outlook.com>
2023-10-27 18:40:27 +0800

    changed input label cases

--- components/ChannelDisplay/Item.vue
+++ components/ChannelDisplay/Item.vue
@@ -10,7 +10,7 @@ import { Title } from '#build/components';
 
 <template>
   <div class="text-sm text-stone-300">
-    <span class="block font-raleway text-xs text-stone-300/70">
+    <span class="block font-raleway text-xs uppercase text-stone-300/70">
       {{ title }}
     </span>
     <span class="font-mono" v-if="itemData">
